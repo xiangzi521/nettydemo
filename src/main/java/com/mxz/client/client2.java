9541dc2dbdbd134d59412d3bf0b0c421658496c0
minxiangzi <534157386@qq.com>
2020-07-09 20:21:53 +0800

    git 测试

---
+++ src/main/java/com/mxz/client/client2.java
@@ -0,0 +1,10 @@
+package com.mxz.client;
+
+/**
+ * Created by Administrator on 2020/7/9.
+ */
+public class client2 {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
